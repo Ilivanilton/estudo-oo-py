1aea63a623da18a9f32cb076ca4cef3b9f8fb63b
ilivanilton <ilivanilton@gmail.com>
2018-11-15 18:56:39 -0300

    pequena melhoria

--- modelos.py
+++ modelos.py
@@ -19,12 +19,17 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
-
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+    
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
+
 
 
 class Serie(Programa):
@@ -32,8 +37,41 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(vingadores.nome)
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self.listagem)
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+demolidor.dar_likes()
+tmep.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
+atlanta.dar_likes()
+
+
+filmes_e_series = [atlanta, vingadores, demolidor, tmep]
+playlist_fds = Playlist('Fim de semana', filmes_e_series)
+
+for programa in playlist_fds.listagem:
+    print(programa)
+
+print(f'Tá ou n tá? {demolidor in playlist_fds.listagem}')
